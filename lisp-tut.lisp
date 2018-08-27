3da4bdeeac1d42f87765ccc2dd1e1380dbfe4491
Neil Grogan <neil@grogan.ie>
2018-08-27 20:20:12 +0100

    if statement updating vars

--- lisp-tut.lisp
+++ lisp-tut.lisp
@@ -100,3 +100,18 @@
     (format t "Work if you want~%")
     (format t "Time for work~%")
 )
+
+(defvar *num* 2)
+(defvar *num-2* 2)
+(defvar *num-3* 2)
+
+(if (= *num* 2)
+    (progn 
+        (setf *num-2* (* *num-2* 2))
+        (setf *num-3* (* *num-3* 3))
+    )
+    (format t "Not equal to 2~%")
+)
+
+(format t "*num-2* = ~d ~%" *num-2*)
+(format t "*num-3* = ~d ~%" *num-3*)
