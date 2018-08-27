b6d3102c2390a864210fe1b9eb5ab73cdf8fa29b
Neil Grogan <neil@grogan.ie>
2018-08-27 19:41:59 +0100

    Added number format and basic maths operations

--- lisp-tut.lisp
+++ lisp-tut.lisp
@@ -6,4 +6,34 @@
     (format t "Hello ~a! ~%" name)
 )
 
+;;; Turn off all caps
+(setq *print-case* :capitalize)
+
 (hello-you *name*)
+
+;;; Define variable
+(defvar *number* 0)
+
+;;; Update variable
+(setf *number* 6)
+
+;;; Formatting Numbers
+(format t "Number with commas ~:d ~%" 10000000)
+
+(format t "PI to 5 charachters ~5f ~%" 3.141593)
+
+(format t "PI to 4 decimals ~,4f ~%" 3.141593)
+
+(format t "10 Percent ~,,2f ~%" .10)
+
+(format t "10 Dollars ~$ ~%" 10)
+
+
+;;; Math
+(format t "(+ 5 4) = ~d ~%" (+ 5 4))
+(format t "(- 5 4) = ~d ~%" (- 5 4))
+(format t "(* 5 4) = ~d ~%" (* 5 4))
+(format t "(/ 5 4) = ~d ~%" (/ 5 4))
+(format t "(/ 5 4.0) = ~d ~%" (/ 5 4.0))
+(format t "(rem 5 4) = ~d ~%" (rem 5 4))
+(format t "(mod 5 4) = ~d ~%" (mod 5 4))
