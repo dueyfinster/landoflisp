29005f434b7f3a05f606923d97bbfad07d3e292d
Neil Grogan <neil@grogan.ie>
2018-08-27 20:42:05 +0100

    Looping through numbers/lists

--- lisp-tut.lisp
+++ lisp-tut.lisp
@@ -157,3 +157,28 @@
     )
     (t (format t "Don't Know ~%"))
 )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; LOOPING
+
+(loop for x from 1 to 10
+    do (print x)
+)
+
+;;; Loop until x is 10, then return
+(setq x 1)
+(loop 
+    (format t "~d ~%" x)
+    (setq x (+ x 1))
+    (when  (> x 10) (return x))
+)
+
+;;; Looping through a list
+(loop for x in '(Peter Paul Mary) do
+    (format t "~s ~%" x)
+)
+
+;; looping starting at arbitrary number
+(loop for y from 100 to 110
+    do (print y)
+)
